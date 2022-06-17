b544d66108806339725e502f0557838f1869ef7f
Aryo Fajar <aryoff@gmail.com>
2022-06-17 13:06:03 +0700

    fix use

--- Http/Controllers/IntegratedAPIController.php
+++ Http/Controllers/IntegratedAPIController.php
@@ -4,6 +4,7 @@ namespace Modules\IntegratedAPI\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class IntegratedAPIController extends Controller
 {
